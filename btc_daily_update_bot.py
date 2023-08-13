bec32682417a095ed942a421e7174503c72a24a0
Chancha <manumcasasgmail.com>
2023-08-13 17:56:55 -0300

    first version to test how to get usd dollar value

--- btc_daily_update_bot.py
+++ btc_daily_update_bot.py
@@ -1,19 +1,34 @@
-import os
+import json
 import telebot
+from drvWebScraping import btc_scraping
 
 usuarios={}
 usuarios['manu']='5178063489'
 
 # we are reading the token from the environmental variables of the OS
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+file= open('settings.json')
+settings = json.load(file)
+
+#BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_TOKEN=settings['TOKEN']
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
+    
+@bot.message_handler(commands=['dollar'])
+def send_welcome(message):
+    usd_prize = btc_scraping()
+    user=message.chat.first_name
+    text = 'Hi ' +user+ ', the prize of the usd is ' + str(usd_prize)
+    bot.reply_to(message, text)
+
+
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
 bot.infinity_polling()
+
